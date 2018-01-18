6d498719bbddd57ecab9382da15783f94495aa3b
Jake Chavez <jake94chavez@gmail.com>
2018-01-18 10:58:50 -0600

    adds Book Schema

---
+++ models/book.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema ({
+	title: {
+		type: String,
+		required: [true, 'This book needs a title']
+	}
+	author: {
+		type: String,
+		required: [true, 'This book needs an author']
+	}
+	releaseDate: String,
+	genres: [{
+		type: String,
+	}]
+	haveRead: {
+		type: Boolean,
+		default: false}
+});
+
+var Book = mongoose.model('Book', BookSchema);
+module.exports = Book;
