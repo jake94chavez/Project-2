dc2c3c933c5ff2480b54cce56e7c0cf1be3d5dcf
Jake Chavez <jake94chavez@gmail.com>
2018-01-23 11:12:03 -0600

    sets up for deployment a sixth time

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var findOrCreate = require('mongoose-findorcreate');
 var expressSession = require('express-session');
 var passport = require('passport');
+var cors = require('cors');
 
 
 mongoose.connect('mongodb://localhost:27017/Langhorne')
@@ -31,6 +32,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', home);
 app.use('/users', users);
