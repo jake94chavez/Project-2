46a21850bb10e9134db2df20065c2ce3c75a814d
Jake Chavez <jake94chavez@gmail.com>
2018-01-25 10:30:13 -0600

    another test

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 var mongoose = require('mongoose');
+let mLabUsername = process.env.MLABUSER;
+let mLabPassword = process.env.MLABPASSWORD;
+let url = 'mongodb://${mLabUsername}:${mLabPassword}@ds045011.mlab.com:45011/heroku_6wm4bl2r'
+// let url = 'mongodb://localhost:27107/Langhorne'
+mongoose.connect(url, 
+				{useMongoClient: true},
+				(err) => {
+					if (err) throw err;
+					else {console.log('connection to db successful');}
+				});
 
 module.exports.Book = require('./book.js')
