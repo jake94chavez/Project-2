8adbafd0766b1e4b953d5b1840c0bb47482342a4
Jake Chavez <jake94chavez@gmail.com>
2018-01-25 10:33:10 -0600

    change db source

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 let mLabUsername = process.env.MLABUSER;
 let mLabPassword = process.env.MLABPASSWORD;
-let url = 'mongodb://${mLabUsername}:${mLabPassword}@ds045011.mlab.com:45011/heroku_6wm4bl2r'
+let url = 'mongodb://${mLabUsername}:${mLabPassword}@ds111568.mlab.com:11568/langhorne'
 // let url = 'mongodb://localhost:27107/Langhorne'
 mongoose.connect(url, 
 				{useMongoClient: true},
